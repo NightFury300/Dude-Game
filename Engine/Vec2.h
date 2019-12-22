6ad6e0026e100d46d6d5ae42ca3a0ab62d264ca1
NightFury <shubhsaxena447@gmail.com>
2019-12-22 15:36:54 +0530

    Added - and -= operators in Vec2 and GetLength(),GetLengthSq(),Normalize(),GetNormalized() in Vec2 Class.

--- Engine/Vec2.h
+++ Engine/Vec2.h
@@ -7,8 +7,14 @@ public:
 	Vec2(float in_x, float in_y);
 	Vec2 operator+(const Vec2 rhs) const;
 	Vec2& operator+=(const Vec2& rhs);
+	Vec2 operator-(const Vec2 rhs) const;
+	Vec2& operator-=(const Vec2& rhs);
 	Vec2 operator*(float rhs) const;
 	Vec2& operator*=(float rhs);
+	float GetLength() const;
+	float GetLengthSq() const;
+	Vec2& Normalize();
+	Vec2 GetNormalized() const;
 
 public:
 	float x;
